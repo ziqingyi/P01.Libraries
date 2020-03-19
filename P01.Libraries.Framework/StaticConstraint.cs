d24ade0b86707928ace75b8c7c33e4d432d0baf3
Adrian Sun <adrian_sun@livingstone.com.au>
2020-03-19 13:13:38 +1100

    start to build factory

---
+++ P01.Libraries.Framework/StaticConstraint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01.Libraries.Framework
+{
+    //read configuration files, static class
+    public class StaticConstraint
+    {
+
+        public readonly static string IBaseDALConfig = ConfigurationManager.AppSettings["IBaseDALConfig"];
+
+    }
+}
