8973ef394e8ddc4500fbc78c2899c03bc3d7af15
adrian sun <ziqingyi@qq.com>
2020-03-22 18:14:10 +1100

    read config

--- P01.Libraries.Framework/StaticConstraint.cs
+++ P01.Libraries.Framework/StaticConstraint.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,23 @@ namespace P01.Libraries.Framework
     //read configuration files, static class
     public class StaticConstraint
     {
-        public readonly static string IBaseDALConfig = ConfigurationManager.AppSettings["IBaseDALConfig"];
+
+        public readonly static string IBaseDALConfig = "";
+        
+        static StaticConstraint()
+        {
+            try
+            {
+                var test = ConfigurationManager.ConnectionStrings["homedb"].ConnectionString; 
+                IBaseDALConfig = test.ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+        }
 
     }
 }
