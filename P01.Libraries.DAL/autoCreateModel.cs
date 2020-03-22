48c397615ee4ef244c658f03851c04e03268b55c
adrian_sun <ziqingyix@gmail.com>
2020-03-22 17:02:47 +1100

    fix project structure

--- P01.Libraries.DAL/autoCreateModel.cs
+++ P01.Libraries.DAL/autoCreateModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
+using P01.Libraries.Framework;
+
 
 namespace P01.Libraries.DAL
 {
@@ -13,7 +16,28 @@ namespace P01.Libraries.DAL
                                                   left join information_schema.key_column_usage b
                                                   on(a.TABLE_NAME = b.TABLE_NAME and a.COLUMN_NAME = b.COLUMN_NAME)";
 
+        public static void BatchmappingModel()
+        {
+            using (SqlConnection conn = new SqlConnection(StaticConstraint.IBaseDALConfig))
+            {
+                using(SqlCommand cmd = new SqlCommand(GetALLTableSql,conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
 
+
+                    }
+
+                }
+            }
+        }
+
+        //generate single object
+        public static void MappingModel(string tablename)
+        {
+
+        }
 
 
 
