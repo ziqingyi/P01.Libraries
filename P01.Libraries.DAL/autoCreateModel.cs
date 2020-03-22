7c3111fe855dbdffd91760d15ce00cd644a4e4e0
adrian_sun <ziqingyix@gmail.com>
2020-03-22 12:14:52 +1100

    auto create model start

---
+++ P01.Libraries.DAL/autoCreateModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Libraries.DAL
+{
+    public static class autoCreateModel
+    {
+        private static string GetALLTableSql = "SELECT name FROM sys.tables WHERE type = 'U'";
+        private static String GetTableInfoSql = @"
+                                                  select a.TABLE_NAME,a.COLUMN_NAME,B.CONSTRAINT_NAME
+                                                  from information_schema.columns a
+                                                  left join information_schema.key_column_usage b
+                                                  on(a.TABLE_NAME = b.TABLE_NAME and a.COLUMN_NAME = b.COLUMN_NAME)";
+
+
+
+
+
+
+    }
+}
