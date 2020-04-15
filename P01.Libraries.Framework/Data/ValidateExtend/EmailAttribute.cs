5e9f4f9018f4ed23bb24c2d2c45df9693190bddd
adrian_sun <ziqingyix@gmail.com>
2020-04-15 20:54:00 +1000

    email validate attribute

---
+++ P01.Libraries.Framework/Data/ValidateExtend/EmailAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace P01.Libraries.Framework.Data.ValidateExtend
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EmailAttribute: AbstractValidateAttribute
+    {
+        private static string EmailRegular = @"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+    
+        
+
+        public override bool Validate(object oValue)
+        {
+            bool test1 = oValue != null;
+            bool test2 = !string.IsNullOrWhiteSpace(oValue.ToString()); 
+            bool test3 = Regex.IsMatch(oValue.ToString(), EmailRegular);
+
+            return test1 && test2 && test3;
+        }
+
+
+
+    }
+}
