2fa576d77014875f77dd06184a26ec4ddeee3d6f
Adrian Sun <adrian_sun@livingstone.com.au>
2020-04-16 09:22:18 +1000

    attribute update

--- P01.Libraries.Framework/Data/ValidateExtend/PhoneAttribute.cs
+++ P01.Libraries.Framework/Data/ValidateExtend/PhoneAttribute.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace P01.Libraries.Framework.Data.ValidateExtend
 {
-    public class PhoneAttribute
+    public class PhoneAttribute : AbstractValidateAttribute
     {
+        private  string _PhoneRegular = @"`[1]+[3,5]+\d[9]";
+        public override bool Validate(object oValue)
+        {
+            bool test1 = oValue != null;
+            bool test2 = !string.IsNullOrWhiteSpace(oValue.ToString());
+            bool test3 = Regex.IsMatch(oValue.ToString(), this._PhoneRegular);
 
-
-
-
+            return test1 && test2 && test3;
+        }
     }
 }
