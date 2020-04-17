e7ffd587ff67a89982329f6a06241284aa265d87
Adrian Sun <adrian_sun@livingstone.com.au>
2020-04-17 17:30:00 +1000

    constructor

--- P01.Libraries.Framework/Data/ValidateExtend/AbstractValidateAttribute.cs
+++ P01.Libraries.Framework/Data/ValidateExtend/AbstractValidateAttribute.cs
@@ -9,6 +9,22 @@ namespace P01.Libraries.Framework.Data.ValidateExtend
 {
     public abstract class AbstractValidateAttribute: System.Attribute
     {
+        private Func<object, ValidateErrorModel> _Func;
+
+        public AbstractValidateAttribute(Func<object, ValidateErrorModel> func)
+        {
+            this._Func = func;
+        }
+        public AbstractValidateAttribute()
+        {}
+        public ValidateErrorModel ValidateSelf(object oValue)
+        {
+            return this._Func.Invoke(oValue);
+        }
+        // sub class don't need to override parent class, 
+        // use delegate to pass to constructor, 
         public abstract ValidateErrorModel Validate(object oValue);
+
+
     }
 }
