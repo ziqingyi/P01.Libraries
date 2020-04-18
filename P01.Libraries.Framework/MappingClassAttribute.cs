33b9c9661e951a74a01f976aa3ded7ae5d80321a
adrian sun <ziqingyi@qq.com>
2020-04-18 23:30:50 +1000

    mapping class

---
+++ P01.Libraries.Framework/MappingClassAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01.Libraries.Framework
+{
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Property)]
+    public class MappingClassAttribute : Attribute
+    {
+        public string MappingName { get; private set; }
+        public MappingClassAttribute(string mappingName)
+        {
+            this.MappingName = mappingName;
+        }
+    }
+}
